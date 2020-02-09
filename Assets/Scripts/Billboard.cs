0d42ee2a78c7bd2e0fba811c2e09b8f4ff0d202b
José Adrián Garrido Puertas <adryx@correo.ugr.es>
2020-02-09 22:30:42 +0100

    UI Selection prepared

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour
+{
+
+    public Camera m_Camera;
+    public bool amActive = false;
+    public bool autoInit = false;
+    GameObject myContainer;
+
+    void Awake()
+    {
+        if (autoInit == true)
+        {
+            m_Camera = Camera.main;
+            amActive = true;
+        }
+    }
+
+
+    void Update()
+    {
+        if (amActive == true)
+        {
+            transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.back, m_Camera.transform.rotation * Vector3.up);
+        }
+    }
+}
